027078d211a1d3e7a61ace4f182db8b92576a7a6
Allen Vailliencourt <chumpmonkey@gmail.com>
2015-12-30 14:56:42 -0500

    Mo' changes.

--- EasyPointTracker/EasyPointTracker/MainPage.xaml.cs
+++ EasyPointTracker/EasyPointTracker/MainPage.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -32,8 +22,7 @@ namespace EasyPointTracker
             this.InitializeComponent();
      
         }
-
-
+        
         // 1st player score panel
         private void downClick1(object sender, RoutedEventArgs e)
         {

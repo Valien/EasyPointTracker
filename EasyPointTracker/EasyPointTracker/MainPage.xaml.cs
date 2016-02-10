150ad62cebeb91087365276a183fa13e44cf299c
Allen Vailliencourt <chumpmonkey@gmail.com>
2016-02-10 14:23:27 -0500

    Now with colors! And removed buggy page...

--- EasyPointTracker/EasyPointTracker/MainPage.xaml.cs
+++ EasyPointTracker/EasyPointTracker/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
@@ -16,19 +17,19 @@ namespace EasyPointTracker
     {
 
         private int defaultValues = 50;
-        //private int initialScorePanelOne = 50;
-        //private int initialScorePanelTwo = 50;
-        
+     
         public MainPage()
         {
             this.InitializeComponent();
-     
+
         }
-        
+
         public void ImgSwap()
         {
+         
             Image img = new Image();
             img.Source = new BitmapImage(new Uri("ms-appx:///Assets/minus-alt.png"));
+                       
         }
 
         // 1st player score panel
@@ -39,30 +40,33 @@ namespace EasyPointTracker
             if (Convert.ToInt32(scorePanelOne.Text) <= defaultValues)
             {
                 downScore -= 1;
-                scorePanelOne.Text = downScore.ToString();             
-            } else
+                scorePanelOne.Text = downScore.ToString();
+                scoreColors();
+            }
+            else
             {
                 downScore -= 1;
                 scorePanelOne.Text = downScore.ToString();
+                scoreColors();
             }
-            //int anInteger;
-            //anInteger = Convert.ToInt32(textBox1.Text);
-            //anInteger = int.Parse(textBox1.Text);
             ImgSwap();
         }
 
         private void upClick1(object sender, RoutedEventArgs e)
         {
-            int upScore = Convert.ToInt32(scorePanelOne.Text); 
+            int upScore = Convert.ToInt32(scorePanelOne.Text);
 
             if (Convert.ToInt32(scorePanelOne.Text) >= defaultValues)
             {
                 upScore += 1;
                 scorePanelOne.Text = upScore.ToString();
-            } else
+                scoreColors();
+            }
+            else
             {
                 upScore += 1;
                 scorePanelOne.Text = upScore.ToString();
+                scoreColors();
             }
         }
 
@@ -75,11 +79,13 @@ namespace EasyPointTracker
             {
                 downScore2 -= 1;
                 scorePanelTwo.Text = downScore2.ToString();
+                scoreColors();
             }
             else
             {
                 downScore2 -= 1;
                 scorePanelTwo.Text = downScore2.ToString();
+                scoreColors();
             }
         }
 
@@ -91,31 +97,60 @@ namespace EasyPointTracker
             {
                 upScore2 += 1;
                 scorePanelTwo.Text = upScore2.ToString();
-
+                scoreColors();
             }
             else
             {
                 upScore2 += 1;
                 scorePanelTwo.Text = upScore2.ToString();
+                scoreColors();
             }
         }
 
+        // compare score panel colors
+        public void scoreColors()
+        {
+            if (scorePanelOne.Text == scorePanelTwo.Text || scorePanelTwo.Text == scorePanelOne.Text)
+            {
+                scorePanelOne.Foreground = new SolidColorBrush(Colors.Blue);
+                scorePanelTwo.Foreground = new SolidColorBrush(Colors.Blue);
+            }
+
+
+            if (Convert.ToInt32(scorePanelOne.Text) < Convert.ToInt32(scorePanelTwo.Text))
+            {
+                scorePanelOne.Foreground = new SolidColorBrush(Colors.Red);
+                scorePanelTwo.Foreground = new SolidColorBrush(Colors.Yellow);
+            }
+
+            if (Convert.ToInt32(scorePanelTwo.Text) < Convert.ToInt32(scorePanelOne.Text))
+            {
+                scorePanelTwo.Foreground = new SolidColorBrush(Colors.Red);
+                scorePanelOne.Foreground = new SolidColorBrush(Colors.Yellow);
+            }
+
+            //(scorePanelOne.Text == scorePanelTwo.Text)
+            //{
+            //    scorePanelOne.Foreground = new SolidColorBrush(Colors.Blue);
+            //}
+        }
         // reset score panels to defaults
         private void resetScorePanelOne(object sender, DoubleTappedRoutedEventArgs e)
         {
             scorePanelOne.Text = defaultValues.ToString();
+            scoreColors();
 
         }
         private void resetScorePanelTwo(object sender, DoubleTappedRoutedEventArgs e)
         {
             scorePanelTwo.Text = defaultValues.ToString();
+            scoreColors();
         }
 
         private void AppBarButton_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(About));
         }
-
 
 
         // somehow it's not pulling variable...must figure out. 
